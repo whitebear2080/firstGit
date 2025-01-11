73b95450cd17a8470f0cfe430042a583e9ac3e88
whitebear2080 <whitebear2080@naver.com>
2025-01-11 16:08:58 +0900

    feature 브런치 추가

---
+++ src/intro/day06/Solution2.java
@@ -0,0 +1,15 @@
+package intro.day06;
+
+/*
+ddddddddddd
+ */
+public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(1));
+    }
+
+    public static int solution(int i) {
+        return i;
+    }
+}
