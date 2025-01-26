c207e475794396b061247bce378f5e6c69972b83
whitebear2080 <whitebear2080@naver.com>
2025-01-26 09:28:14 +0900

    feat: basic day22
    basic day22

---
+++ src/basic/day22/Solution3.java
@@ -0,0 +1,34 @@
+package basic.day22;
+
+/*
+문자열로 변환
+제출 내역
+문제 설명
+정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 10000
+입출력 예
+n	result
+123	"123"
+2573	"2573"
+입출력 예 설명
+입출력 예 #1
+
+123을 문자열로 변환한 "123"을 return합니다.
+입출력 예 #2
+
+2573을 문자열로 변환한 "2573"을 return합니다.
+ */
+
+public class Solution3 {
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+        System.out.println(solution(2573));
+    }
+
+    public static String solution(int n) {
+        return String.valueOf(n);
+    }
+}
+
