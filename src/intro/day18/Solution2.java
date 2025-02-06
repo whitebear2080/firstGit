92367c2630b32b9cdc68eecc649e840321a9f9a2
whitebear2080 <whitebear2080@naver.com>
2025-02-07 00:04:31 +0900

    feat: intro day18
    intro day18

---
+++ src/intro/day18/Solution2.java
@@ -0,0 +1,33 @@
+package intro.day18;
+
+/*
+제곱수 판별하기
+제출 내역
+문제 설명
+어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+1 ≤ n ≤ 1,000,000
+입출력 예
+n	result
+144	1
+976	2
+입출력 예 설명
+입출력 예 #1
+
+144는 12의 제곱이므로 제곱수입니다. 따라서 1을 return합니다.
+입출력 예 #2
+
+976은 제곱수가 아닙니다. 따라서 2를 return합니다.
+*/
+public class Solution2 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(144));
+        System.out.println(solution(976));
+    }
+
+    public static int solution(int n) {
+        return Math.sqrt(n) % 1 == 0.0 ? 1 : 2;
+    }
+}
